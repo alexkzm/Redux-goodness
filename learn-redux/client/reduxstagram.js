02801e3393e83999043c7c4dc10068fea0df7707
Alexander Kouzmine <alexkzm@gmail.com>
2016-07-07 13:20:03 -0400

    more components

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import css from './styles/style.styl'
 
 import App from './components/App'
-import Single form './componets/Single'
+import Single from './componets/Single'
 import PhotoGrid from './components/PhotoGrid'
 
 import { Router, Route, IndexRoute, browserHistory} from 'react-router'
